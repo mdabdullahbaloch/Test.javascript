cc4e753e8bf1b17751af0f5add2d4ff2c66ecb12
Abdullah <80870788+mdabdullahbaloch@users.noreply.github.com>
2023-10-18 11:51:46 +0000

    conversionOperation

---
+++ basics/conversionOperation.js
@@ -0,0 +1,16 @@
+// let score = true
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log( valueInNumber);
+// "33" => 33
+//"33abc" => NAN
+//true => 1; false=>0
+let isLoggedIn  = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1=> true ; 0 =>false
+//"" => false
+// "abdullah" => true
+
